9d180b7795969f2f9f47f0ca0de59557734128a4
dominic.griesel <d.griesel@gmx.net>
2017-09-05 15:18:52 +0200

    first version of admin tab

--- admin/words.js
+++ admin/words.js
@@ -1,8 +1,17 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"template adapter settings": {"de": "Beispiel", "ru": "Пример"},
-	"test1":                    {"en": "Test 1", "de": "Test 1", "ru": "Тест 1"},
-	"test2":                    {"en": "Test 2", "de": "Test 2", "ru": "Тест 2"},
+	"BLE adapter settings": {
+		"de": "Einstellungen f?r BLE Adapter"
+	},
+	"Settings": {
+		"de": "Einstellungen"
+	},
+	"Monitored Services:": {
+		"de": "?berwachte Dienste:"
+	},
+	"Service characteristics as HEX codes or UUID, comma separated.": {
+		"de": "Dienstcharakteristiken als HEX oder UUID, durch Komma getrennt."
+	},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
 		"ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
