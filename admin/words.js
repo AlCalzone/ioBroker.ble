15dd3a66a17262a01b930951d37db4099f80abbf
Jey-Cee <(jey-cee@live.com)>
2017-09-25 12:23:29 +0200

    initial release

--- admin/words.js
+++ admin/words.js
@@ -1,6 +1,6 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"template adapter settings": {"de": "Beispiel", "ru": "Пример"},
+	"EnOcean adapter settings": {"de": "Beispiel", "ru": "Пример"},
 	"test1":                    {"en": "Test 1", "de": "Test 1", "ru": "Тест 1"},
 	"test2":                    {"en": "Test 2", "de": "Test 2", "ru": "Тест 2"},
 	"on save adapter restarts with new config immediately": {
