e9fe4992318797f9d20ed6f26accbad7b4116293
Jey-Cee <christoph@von-gerle.de>
2017-09-26 23:26:13 +0200

    Add USB dropdown test

--- admin/words.js
+++ admin/words.js
@@ -4,6 +4,7 @@ systemDictionary = {
     "teachIn":                    {"en": "Teach mode", "de": "Anlernmodus", "ru": "режим обучения"},
     "Timeout":                    {"en": "Timeout", "de": "Timeout", "ru": "Тайм-аут"},
     "USB name:":                 {"en": "USB name:", "de": "USB Name:", "ru": "Имя USB"},
+    "Serialport":                 {"en": "Serialport:", "de": "Serialport", "ru": "Имя USB"},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
 		"ru": "При сохранении настроек адаптера он сразу же перезапускается"
