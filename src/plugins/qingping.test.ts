4e78b2dfa410ce48a2e3517d52f4d66b9d3ecd63
xhunter74 <xhunter74@gmail.com>
2024-12-13 16:04:01 +0200

    Updated tests.

--- src/plugins/qingping.test.ts
+++ src/plugins/qingping.test.ts
@@ -1,53 +1,56 @@
 import { expect } from "chai";
-import sinon from "sinon";
 import plugin from "./qingping";
-import { Global as _ } from "../lib/global";
-import { PeripheralInfo } from "../lib/scanProcessInterface";
 
 describe("qingping plugin => ", () => {
 	describe("correctly detects which peripherals should be handled => ", () => {
 		it("yes: Valid advertisement data", () => {
-			const peripheral: PeripheralInfo = {
+			const peripheral = {
 				advertisement: {
 					serviceData: [
 						{
 							uuid: "fdcd",
-							data: Buffer.from("71205b04d4281061c8593f090610023002", "hex"),
+							data: Buffer.from(
+								"08108b8182342d580104e300aa0102014d",
+								"hex",
+							),
 						},
 					],
 				},
 				address: "the:address:no:longer:matters",
 			};
-			expect(plugin.isHandling(peripheral)).to.be.true;
+			expect(plugin.isHandling(peripheral as any)).to.be.true;
 		});
 
 		it("yes: The service data is not tested again after a successful check", () => {
-			const peripheral: PeripheralInfo = {
+			const peripheral = {
 				advertisement: {
 					serviceData: [{ uuid: "fdcd", data: Buffer.from([]) }],
 				},
 				address: "the:address:no:longer:matters",
 			};
-			expect(plugin.isHandling(peripheral)).to.be.true;
+			expect(plugin.isHandling(peripheral as any)).to.be.true;
 		});
 
 		it("no: Valid advertisement data, but wrong UUID", () => {
-			const peripheral: PeripheralInfo = {
+			const peripheral = {
 				advertisement: {
 					serviceData: [
 						{
 							uuid: "fdce",
-							data: Buffer.from("71205b04d4281061c8593f090610023002", "hex"),
+							data: Buffer.from(
+								"71205b04d4281061c8593f090610023002",
+								"hex",
+							),
 						},
 					],
 				},
 				address: "the:address:no:longer:matters",
 			};
-			expect(plugin.isHandling(peripheral)).to.be.false;
+			expect(plugin.isHandling(peripheral as any)).to.be.false;
 		});
 
 		it("no: invalid advertisement data", () => {
-			const peripheral: PeripheralInfo = {
+			const peripheral = {
 				advertisement: {
 					serviceData: [
 						{
@@ -58,37 +61,40 @@ describe("qingping plugin => ", () => {
 				},
 				address: "the:address:no:longer:matters",
 			};
-			expect(plugin.isHandling(peripheral)).to.be.false;
+			expect(plugin.isHandling(peripheral as any)).to.be.false;
 		});
 
 		it("no: no service data", () => {
-			const peripheral: PeripheralInfo = {
+			const peripheral = {
 				advertisement: {},
 				address: "the:address:no:longer:matters",
 			};
-			expect(plugin.isHandling(peripheral)).to.be.false;
+			expect(plugin.isHandling(peripheral as any)).to.be.false;
 		});
 	});
 
 	describe("createContext => ", () => {
 		it("creates context with valid data", () => {
-			const peripheral: PeripheralInfo = {
+			const peripheral = {
 				advertisement: {
 					serviceData: [
 						{
 							uuid: "fdcd",
-							data: Buffer.from("71205b04d4281061c8593f090610023002", "hex"),
+							data: Buffer.from(
+								"71205b04d4281061c8593f090610023002",
+								"hex",
+							),
 						},
 					],
 				},
 				address: "the:address:no:longer:matters",
 			};
-			const context = plugin.createContext(peripheral);
+			const context = plugin.createContext(peripheral as any);
 			expect(context).to.have.property("event");
 		});
 
 		it("returns undefined for invalid data", () => {
-			const peripheral: PeripheralInfo = {
+			const peripheral = {
 				advertisement: {
 					serviceData: [
 						{
@@ -99,7 +105,7 @@ describe("qingping plugin => ", () => {
 				},
 				address: "the:address:no:longer:matters",
 			};
-			const context = plugin.createContext(peripheral);
+			const context = plugin.createContext(peripheral as any);
 			expect(context).to.be.undefined;
 		});
 	});
@@ -119,7 +125,7 @@ describe("qingping plugin => ", () => {
 		});
 
 		it("returns undefined for undefined context", () => {
-			const objects = plugin.defineObjects(undefined);
+			const objects = plugin.defineObjects(undefined as any);
 			expect(objects).to.be.undefined;
 		});
 	});
@@ -138,7 +144,7 @@ describe("qingping plugin => ", () => {
 		});
 
 		it("returns undefined for undefined context", () => {
-			const values = plugin.getValues(undefined);
+			const values = plugin.getValues(undefined as any);
 			expect(values).to.be.undefined;
 		});
 	});
